eefa2fd1e2225d6e0f044fbc0211e4ed4a813e44
hujun <hujun3@xiaomi.com>
2023-02-11 21:46:35 +0800

    logback adapter

---
+++ src/main/java/com/alibaba/nacos/logbackadpater/NacosClientPropertyModelAction.java
@@ -0,0 +1,46 @@
+/*
+ *
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.alibaba.nacos.logbackadpater;
+
+import ch.qos.logback.core.joran.action.BaseModelAction;
+import ch.qos.logback.core.joran.spi.SaxEventInterpretationContext;
+import ch.qos.logback.core.model.Model;
+import org.xml.sax.Attributes;
+
+/**
+ * Logback model action to support <nacosClientProperty/> tags.
+ *
+ * @author hujun
+ */
+public class NacosClientPropertyModelAction extends BaseModelAction {
+    private static final String SOURCE_ATTRIBUTE = "source";
+    
+    private static final String DEFAULT_VALUE_ATTRIBUTE = "defaultValue";
+    
+    @Override
+    protected Model buildCurrentModel(SaxEventInterpretationContext interpretationContext, String name,
+            Attributes attributes) {
+        NacosClientPropertyModel model = new NacosClientPropertyModel();
+        model.setName(attributes.getValue(NAME_ATTRIBUTE));
+        model.setSource(attributes.getValue(SOURCE_ATTRIBUTE));
+        model.setScope(attributes.getValue(SCOPE_ATTRIBUTE));
+        model.setDefaultValue(attributes.getValue(DEFAULT_VALUE_ATTRIBUTE));
+        return model;
+    }
+}
---
+++ src/main/java/com/alibaba/nacos/logbackadpater/NacosLogbackConfiguratorAdapterV2.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.nacos.logbackadpater;
+
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.Context;
+import ch.qos.logback.core.joran.spi.ElementSelector;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.joran.spi.RuleStore;
+import ch.qos.logback.core.model.Model;
+import ch.qos.logback.core.model.processor.DefaultProcessor;
+import com.alibaba.nacos.common.log.NacosLogbackConfigurator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * ensure that Nacos configuration does not affect user configuration savepoints and  scanning url.
+ *
+ * @author hujun
+ */
+public class NacosLogbackConfiguratorAdapterV2 extends JoranConfigurator implements NacosLogbackConfigurator {
+    
+    /**
+     * ensure that Nacos configuration does not affect user configuration savepoints.
+     *
+     * @param top safe data
+     */
+    @Override
+    public void registerSafeConfiguration(Model top) {
+    }
+    
+    protected void addModelHandlerAssociations(DefaultProcessor defaultProcessor) {
+        defaultProcessor.addHandler(NacosClientPropertyModel.class,
+                (handlerContext, handlerMic) -> new NacosClientPropertyModelHandler(this.context));
+        super.addModelHandlerAssociations(defaultProcessor);
+    }
+    
+    public void addElementSelectorAndActionAssociations(RuleStore ruleStore) {
+        super.addElementSelectorAndActionAssociations(ruleStore);
+        ruleStore.addRule(new ElementSelector("configuration/nacosClientProperty"),
+                NacosClientPropertyModelAction::new);
+    }
+    
+    @Override
+    public int getVersion() {
+        return 2;
+    }
+    
+    @Override
+    public void setContext(Object loggerContext) {
+        super.setContext((Context) loggerContext);
+    }
+    
+    @Override
+    public void configure(URL url) throws Exception {
+        InputStream in = null;
+        try {
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setUseCaches(false);
+            in = urlConnection.getInputStream();
+            doConfigure(in, url.toExternalForm());
+        } catch (IOException ioe) {
+            String errMsg = "Could not open URL [" + url + "].";
+            addError(errMsg, ioe);
+            throw new JoranException(errMsg, ioe);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ioe) {
+                    String errMsg = "Could not close input stream";
+                    addError(errMsg, ioe);
+                    throw new JoranException(errMsg, ioe);
+                }
+            }
+        }
+    }
+}
